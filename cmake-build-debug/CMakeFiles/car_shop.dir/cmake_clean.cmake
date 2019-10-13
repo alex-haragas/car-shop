6838d3b5fc5d788a7a8a0437092b65a42b9a225b
Alex <haragas6@gmail.com>
2019-10-13 09:51:15 +0300

    car-shop

---
+++ cmake-build-debug/CMakeFiles/car_shop.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/car_shop.dir/main.c.obj"
+  "car_shop.exe"
+  "car_shop.exe.manifest"
+  "car_shop.pdb"
+  "libcar_shop.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/car_shop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
